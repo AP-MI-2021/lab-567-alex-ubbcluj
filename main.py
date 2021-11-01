75764dd26764fa53b76943fb3ef7bc8491fda250
alex-ubbcluj <alexandru.gruia1@stud.ubbcluj.ro>
2021-11-01 15:47:18 +0200

    seventh commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Tests.testAll import run_all_tests
 from UI.console import run_meniu
+from UI.commandLine import run_command_line
 
 
 def main():
     run_all_tests()
-    run_meniu([])
+    run_command_line([])
 
 
 if __name__ == "__main__":
