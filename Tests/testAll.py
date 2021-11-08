200312d4563cf1f6174e91a105153c9aa783f551
alex-ubbcluj <alexandru.gruia1@stud.ubbcluj.ro>
2021-11-08 15:48:46 +0200

    tenth commit

--- Tests/testAll.py
+++ Tests/testAll.py
@@ -1,6 +1,7 @@
 from Tests.testCRUD import test_adauga_obiect, test_get_by_ID, test_sterge_obiect, test_modifica_obiect
 from Tests.testDomain import test_creeaza_obiect
 from Tests.testFunctionalitati import test_concatenare_str, test_ordonare_cresc_dupa_pret, test_pret_max_fiecare_locatie, test_suma_fiecare_locatie, test_mutare_obiecte
+from Tests.testUndoRedo import test_undo_redo
 
 
 def run_all_tests():
@@ -14,3 +15,4 @@ def run_all_tests():
     test_pret_max_fiecare_locatie()
     test_ordonare_cresc_dupa_pret()
     test_suma_fiecare_locatie()
+    test_undo_redo()
